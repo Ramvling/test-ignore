02bfb423428424887456b9da70ac36b48adc6b6b
thinktt <github@thinktt.e4ward.com>
2016-09-07 11:28:40 -0500

    setting up report-web-dashboard

---
+++ public/app.js
@@ -0,0 +1,20 @@
+let data = {};
+data.name = 'Toby';
+
+class Layout extends React.Component {
+  render() {
+    return(<h1>It works!</h1>);
+  }
+}
+
+let component = <h1>Yo hum hum!</h1>; 
+
+
+ReactDOM.render(
+  component,
+  document.getElementById('example')
+);
+
+
+
+
